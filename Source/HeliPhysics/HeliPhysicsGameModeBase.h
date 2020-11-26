171d1715a787faa0b3643fdebb0a7bd91972a098
pankaxz <pankajpanwar3.98@gmail.com>
2020-11-27 02:51:38 +0530

    Initial commit

---
+++ Source/HeliPhysics/HeliPhysicsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HeliPhysicsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELIPHYSICS_API AHeliPhysicsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
