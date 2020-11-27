91be7758425aa377617e8e1c84b2a7a458407c48
pankaxz <pankajpanwar3.98@gmail.com>
2020-11-28 03:11:27 +0530

    added working inputs

--- Source/HeliPhysics/R22Heli_Pawn.cpp
+++ Source/HeliPhysics/R22Heli_Pawn.cpp
@@ -10,61 +10,60 @@ AR22Heli_Pawn::AR22Heli_Pawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
 	//Physics box
-	SM_PhysicsBox = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM_PhysicsBox"));
-	RootComponent = SM_PhysicsBox;
-	SM_PhysicsBox->SetSimulatePhysics(true);
-	SM_PhysicsBox->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
-	SM_PhysicsBox->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
-	SM_PhysicsBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SM_RootBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM_RootBody"));
+	RootComponent = SM_RootBody;
+	SM_RootBody->SetSimulatePhysics(true);
+	SM_RootBody->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
+	SM_RootBody->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+	SM_RootBody->SetCollisionResponseToAllChannels(ECR_Ignore);
 	//SM_PhysicsBox->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 	
 
 	//Graphics group
 	SC_Graphics = CreateDefaultSubobject<USceneComponent>(TEXT("SC_Graphics"));
-	SC_Graphics->SetupAttachment(SM_PhysicsBox);
+	SC_Graphics->SetupAttachment(SM_RootBody);
 	
 	//Colliders
-	COL_Fuselage = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_Fuselage"));
-	COL_Fuselage->SetupAttachment(SM_PhysicsBox);
-	COL_Fuselage->SetCapsuleRadius(16.0f);
-	COL_Fuselage->SetCapsuleHalfHeight(26.0f);
-	COL_Fuselage->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-	COL_Fuselage->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
-	COL_Fuselage->SetCollisionResponseToAllChannels(ECR_Block);
+	Col_Fuselage = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_Fuselage"));
+	Col_Fuselage->SetupAttachment(SM_RootBody);
+	Col_Fuselage->SetCapsuleRadius(16.0f);
+	Col_Fuselage->SetCapsuleHalfHeight(26.0f);
+	Col_Fuselage->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Col_Fuselage->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
+	Col_Fuselage->SetCollisionResponseToAllChannels(ECR_Block);
 	
-	COL_Mast = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_Mast"));
-	COL_Mast->SetupAttachment(SM_PhysicsBox);
-	COL_Mast->SetCapsuleRadius(7.0f);
-	COL_Mast->SetCapsuleHalfHeight(15.0f);
-	COL_Mast->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-	COL_Mast->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
-	COL_Mast->SetCollisionResponseToAllChannels(ECR_Block);
+	Col_Mast = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_Mast"));
+	Col_Mast->SetupAttachment(SM_RootBody);
+	Col_Mast->SetCapsuleRadius(7.0f);
+	Col_Mast->SetCapsuleHalfHeight(15.0f);
+	Col_Mast->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Col_Mast->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
+	Col_Mast->SetCollisionResponseToAllChannels(ECR_Block);
 	
-	COL_Tail = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_Tail"));
-	COL_Tail->SetupAttachment(SM_PhysicsBox);
-	COL_Tail->SetCapsuleRadius(4.0f);
-	COL_Tail->SetCapsuleHalfHeight(21.0f);
-	COL_Tail->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-	COL_Tail->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
-	COL_Tail->SetCollisionResponseToAllChannels(ECR_Block);
+	Col_Tail = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_Tail"));
+	Col_Tail->SetupAttachment(SM_RootBody);
+	Col_Tail->SetCapsuleRadius(4.0f);
+	Col_Tail->SetCapsuleHalfHeight(21.0f);
+	Col_Tail->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Col_Tail->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
+	Col_Tail->SetCollisionResponseToAllChannels(ECR_Block);
 
-	COL_LSkid = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_LSkid"));
-	COL_LSkid->SetupAttachment(SM_PhysicsBox);
-	COL_LSkid->SetCapsuleRadius(1.0f);
-	COL_LSkid->SetCapsuleHalfHeight(20.0f);
-	COL_LSkid->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-	COL_LSkid->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
-	COL_LSkid->SetCollisionResponseToAllChannels(ECR_Block);
+	Col_LSkid = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_LSkid"));
+	Col_LSkid->SetupAttachment(SM_RootBody);
+	Col_LSkid->SetCapsuleRadius(1.0f);
+	Col_LSkid->SetCapsuleHalfHeight(20.0f);
+	Col_LSkid->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Col_LSkid->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
+	Col_LSkid->SetCollisionResponseToAllChannels(ECR_Block);
 
-	COL_RSkid = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_RSkid"));
-	COL_RSkid->SetupAttachment(SM_PhysicsBox);
-	COL_RSkid->SetCapsuleRadius(1.0f);
-	COL_RSkid->SetCapsuleHalfHeight(20.0f);
-	COL_RSkid->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-	COL_RSkid->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
-	COL_RSkid->SetCollisionResponseToAllChannels(ECR_Block);
+	Col_RSkid = CreateDefaultSubobject<UCapsuleComponent>(TEXT("COL_RSkid"));
+	Col_RSkid->SetupAttachment(SM_RootBody);
+	Col_RSkid->SetCapsuleRadius(1.0f);
+	Col_RSkid->SetCapsuleHalfHeight(20.0f);
+	Col_RSkid->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Col_RSkid->SetCollisionObjectType(ECollisionChannel::ECC_Vehicle);
+	Col_RSkid->SetCollisionResponseToAllChannels(ECR_Block);
 }
 
 // Called when the game starts or when spawned
@@ -85,6 +84,79 @@ void AR22Heli_Pawn::Tick(float DeltaTime)
 void AR22Heli_Pawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	
+	PlayerInputComponent->BindAxis(TEXT("Horizontal"), this, &AR22Heli_Pawn::SetHorizontalInput);
+	PlayerInputComponent->BindAxis(TEXT("Vertical"), this, &AR22Heli_Pawn::SetVerticalInput);
+	PlayerInputComponent->BindAxis(TEXT("Throttle"), this, &AR22Heli_Pawn::SetThrottleInput);
+	PlayerInputComponent->BindAxis(TEXT("Collective"), this, &AR22Heli_Pawn::SetCollectiveInput);
+	PlayerInputComponent->BindAxis(TEXT("Pedal"), this, &AR22Heli_Pawn::SetPedalInput);
 }
 
+
+//Setter Functions
+
+void AR22Heli_Pawn::SetHorizontalInput(float AxisValue)
+{
+	HorizontalInput = AxisValue;
+	CyclicInput.X = HorizontalInput;
+}
+
+void AR22Heli_Pawn::SetVerticalInput(float AxisValue)
+{
+	VerticalInput = AxisValue;
+	CyclicInput.Y = VerticalInput;
+}
+
+void AR22Heli_Pawn::SetThrottleInput(float AxisValue)
+{
+	ThrottleInput = AxisValue;
+}
+
+void AR22Heli_Pawn::SetPedalInput(float AxisValue)
+{
+	PedalInput = AxisValue;
+}
+
+void AR22Heli_Pawn::SetCollectiveInput(float AxisValue)
+{
+	CollectiveInput = AxisValue;
+}
+
+void AR22Heli_Pawn::SetCyclicInput()
+{
+	CyclicInput.Y = VerticalInput;
+	CyclicInput.X = HorizontalInput;
+}
+
+
+//Getter Functions
+
+float AR22Heli_Pawn::GetHorizontalInput()
+{
+	return HorizontalInput;
+}
+
+float AR22Heli_Pawn::GetVerticalInput()
+{
+	return  VerticalInput;
+}
+
+float AR22Heli_Pawn::GetThrottleInput()
+{
+	return ThrottleInput;
+}
+
+float AR22Heli_Pawn::GetPedalInput()
+{
+	return PedalInput;
+}
+
+float AR22Heli_Pawn::GetCollectiveInput()
+{
+	return CollectiveInput;
+}
+
+FVector2D AR22Heli_Pawn::GetCyclicInput()
+{
+	return CyclicInput;
+}
--- Source/HeliPhysics/R22Heli_Pawn.h
+++ Source/HeliPhysics/R22Heli_Pawn.h
@@ -16,30 +16,39 @@ public:
 	// Sets default values for this pawn's properties
 	AR22Heli_Pawn();
 
+	//Getters
+	float GetHorizontalInput();
+	float GetVerticalInput() ;
+	float GetThrottleInput();
+	float GetPedalInput();
+	float GetCollectiveInput();
+	FVector2D GetCyclicInput();
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	
-    UPROPERTY(VisibleAnywhere, Category="HeliPawn")
-    UStaticMeshComponent* SM_PhysicsBox;
+    UPROPERTY(EditDefaultsOnly, Category="HeliPawn")
+    UStaticMeshComponent* SM_RootBody;
     
-    UPROPERTY(VisibleAnywhere, Category="HeliPawn")
+    UPROPERTY()
     USceneComponent* SC_Graphics;
     
-    UPROPERTY(VisibleAnywhere, Category="HeliPawn")
-    UCapsuleComponent* COL_Mast;
+    UPROPERTY(EditDefaultsOnly)
+    UCapsuleComponent* Col_Mast;
     
-    UPROPERTY(VisibleAnywhere, Category="HeliPawn")
-    UCapsuleComponent* COL_RSkid;
+    UPROPERTY(EditDefaultsOnly)
+    UCapsuleComponent* Col_RSkid;
     
-    UPROPERTY(VisibleAnywhere, Category="HeliPawn")
-    UCapsuleComponent* COL_LSkid;
+    UPROPERTY(EditDefaultsOnly)
+    UCapsuleComponent* Col_LSkid;
     
-    UPROPERTY(VisibleAnywhere, Category="HeliPawn")
-    UCapsuleComponent* COL_Tail;
+    UPROPERTY(EditDefaultsOnly)
+    UCapsuleComponent* Col_Tail;
+    
+    UPROPERTY(EditDefaultsOnly)
+    UCapsuleComponent* Col_Fuselage;
 
-    UPROPERTY(VisibleAnywhere, Category="HeliPawn")
-    UCapsuleComponent* COL_Fuselage;
 
 public:	
 	// Called every frame
@@ -47,5 +56,40 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	//Setters
+
+	void SetHorizontalInput(float AxisValue);
+	void SetVerticalInput(float AxisValue);
+	void SetThrottleInput(float AxisValue);
+	void SetPedalInput(float AxisValue);
+	void SetCollectiveInput(float AxisValue);
+	void SetCyclicInput();
+	//Input Variables
+	//
+	//Horizontal Input - w s
+	float HorizontalInput = 0.0f;
+	
+	//Vertical Input - a d
+	float VerticalInput = 0.0f;
+	
+	//Throttle Input 9 0 
+	UPROPERTY(VisibleAnywhere, Category = "InputTest")
+	float ThrottleInput = 0.0f;
+	
+	//Pedal Input - right left
+	UPROPERTY(VisibleAnywhere, Category = "InputTest")
+	float PedalInput = 0.0f;
+	
+	//Collective Input - up down
+	UPROPERTY(VisibleAnywhere, Category = "InputTest")
+	float CollectiveInput = 0.0f;
+	
+	// Cyclic input - X = Horizontal Input			
+	// Cyclic input - Y = Vertical Input			
+	UPROPERTY(VisibleAnywhere, Category = "InputTest")
+	FVector2D CyclicInput = FVector2D().ZeroVector;
+		
 
 };
